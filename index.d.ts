c9879708b3e4ee54c75416b58d2d0b90e8acd8e2
Leonardo Dino <leonardo@kunstdesign.com.br>
2017-10-09 01:55:14 -0300

    update documentation

--- index.d.ts
+++ index.d.ts
@@ -4,19 +4,18 @@ import * as ReactNative from "react-native";
 declare module "react-native-confirmation-code-input" {
 
     type InputPositions = 'left' | 'right' | 'center' | 'full-width';
-    type ClassNames = 'border-box' | 'border-circle' | 'border-b' | 'border-b-t' | 'border-l-r';
+    type BorderTypes = 'clear' | 'square' | 'circle' | 'underline';
 
     interface CodeInputProps extends ReactNative.TextInputProperties {
+        inputComponent?: Function;
         codeLength?: number;
-        compareWithCode?: string;
         inputPosition?: InputPositions;
         size?: number;
         space?: number;
-        className?: ClassNames;
+        borderType?: BorderTypes;
         cellBorderWidth?: number;
         activeColor?: string;
         inactiveColor?: string;
-        ignoreCase?: boolean;
         codeInputStyle?: any,
         containerStyle?: any;
         onFulfill: Function;
