f43850ae874d5fa92af43aadb5c55d2b4c5b4af0
Web & Mobile Developer <top.webdeveloper2@gmail.com>
2017-12-12 23:57:06 +0400

    getCode() added
    This is for getting Code on current state

--- components/ConfirmationCodeInput.js
+++ components/ConfirmationCodeInput.js
@@ -102,6 +102,10 @@ export default class ConfirmationCodeInput extends Component {
 		})
 		this._setFocus(0)
 	}
+	getCode = () => {
+		var code = this.state.codeArr.fill('')
+		return code
+	}
 
 	_setFocus = (index) => this.codeInputRefs[index].focus()
 
